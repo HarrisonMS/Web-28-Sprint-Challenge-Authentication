54c0ddb96b45962f72a71bbdaac38755450f49e0
Harrison <hmseaborn@gmail.com>
2020-04-24 08:08:10 -0700

    dependencies wanted installed

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,20 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
+const secrets = require('../api/secrets')
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization
+	if (token) {
+    console.log(token)
+		const secret = secrets.jwtSecret;
+		jwt.verify(token, secret, function(err, decodedToken) {
+			if (err) {
+				res.status(401).json({ message: 'invalid token oof' });
+			} else {
+				req.token = decodedToken;
+				next();
+			}
+		});
+	} else {
+		res.status(401).json({ message: 'Please login and try again!' });
+	}
 };
